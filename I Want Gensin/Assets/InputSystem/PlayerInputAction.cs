a196821c8b1b08f56dc485731a208c2e2e50024d
JM991103 <ljn1103@naver.com>
2023-05-19 17:36:53 +0900

    애니메이션 수정 및 달리기 추가

--- I Want Gensin/Assets/InputSystem/PlayerInputAction.cs
+++ I Want Gensin/Assets/InputSystem/PlayerInputAction.cs
@@ -37,6 +37,15 @@ public partial class @PlayerInputAction : IInputActionCollection2, IDisposable
                     ""initialStateCheck"": true
                 },
                 {
+                    ""name"": ""MoveModeChange"",
+                    ""type"": ""Button"",
+                    ""id"": ""4e54dcee-ed03-4ff9-9644-e98b1ba2df1b"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
                     ""name"": ""Attack"",
                     ""type"": ""Button"",
                     ""id"": ""021677bc-824d-46ea-a75e-a60e02fb2d8e"",
@@ -112,6 +121,17 @@ public partial class @PlayerInputAction : IInputActionCollection2, IDisposable
                     ""action"": ""Move"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a6aff9bb-e0be-46c2-9d07-1b4acdb011d2"",
+                    ""path"": ""<Keyboard>/leftShift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MoveModeChange"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -138,6 +158,7 @@ public partial class @PlayerInputAction : IInputActionCollection2, IDisposable
         // Player
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+        m_Player_MoveModeChange = m_Player.FindAction("MoveModeChange", throwIfNotFound: true);
         m_Player_Attack = m_Player.FindAction("Attack", throwIfNotFound: true);
     }
 
@@ -199,12 +220,14 @@ public partial class @PlayerInputAction : IInputActionCollection2, IDisposable
     private readonly InputActionMap m_Player;
     private IPlayerActions m_PlayerActionsCallbackInterface;
     private readonly InputAction m_Player_Move;
+    private readonly InputAction m_Player_MoveModeChange;
     private readonly InputAction m_Player_Attack;
     public struct PlayerActions
     {
         private @PlayerInputAction m_Wrapper;
         public PlayerActions(@PlayerInputAction wrapper) { m_Wrapper = wrapper; }
         public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputAction @MoveModeChange => m_Wrapper.m_Player_MoveModeChange;
         public InputAction @Attack => m_Wrapper.m_Player_Attack;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
@@ -218,6 +241,9 @@ public partial class @PlayerInputAction : IInputActionCollection2, IDisposable
                 @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
                 @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
                 @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @MoveModeChange.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMoveModeChange;
+                @MoveModeChange.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMoveModeChange;
+                @MoveModeChange.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMoveModeChange;
                 @Attack.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack;
                 @Attack.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack;
                 @Attack.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack;
@@ -228,6 +254,9 @@ public partial class @PlayerInputAction : IInputActionCollection2, IDisposable
                 @Move.started += instance.OnMove;
                 @Move.performed += instance.OnMove;
                 @Move.canceled += instance.OnMove;
+                @MoveModeChange.started += instance.OnMoveModeChange;
+                @MoveModeChange.performed += instance.OnMoveModeChange;
+                @MoveModeChange.canceled += instance.OnMoveModeChange;
                 @Attack.started += instance.OnAttack;
                 @Attack.performed += instance.OnAttack;
                 @Attack.canceled += instance.OnAttack;
@@ -247,6 +276,7 @@ public partial class @PlayerInputAction : IInputActionCollection2, IDisposable
     public interface IPlayerActions
     {
         void OnMove(InputAction.CallbackContext context);
+        void OnMoveModeChange(InputAction.CallbackContext context);
         void OnAttack(InputAction.CallbackContext context);
     }
 }
--- I Want Gensin/Assets/Scripts/Player/PlayerController.cs
+++ I Want Gensin/Assets/Scripts/Player/PlayerController.cs
@@ -9,10 +9,14 @@ public class PlayerController : MonoBehaviour
     PlayerInputAction inputActions;
     CharacterController cc;
     Animator anim;
+    Transform player;
 
     Vector3 inputDir;
     public float currentSpeed = 3.0f;
+    public float walkSpeed = 3.0f;
+    public float runSpeed = 5.0f;
     Quaternion targetRotation = Quaternion.identity;
+    bool isMoveChange = false;
 
     enum MoveMode
     {
@@ -27,6 +31,8 @@ public class PlayerController : MonoBehaviour
         inputActions = new PlayerInputAction();
         cc = GetComponent<CharacterController>();
         anim = GetComponentInChildren<Animator>();
+
+        player = GetComponentInChildren<Transform>();
     }
 
     private void OnEnable()
@@ -34,12 +40,16 @@ public class PlayerController : MonoBehaviour
         inputActions.Player.Enable();
         inputActions.Player.Move.performed += OnMove;
         inputActions.Player.Move.canceled += OnMove;
+        inputActions.Player.MoveModeChange.performed += OnMoveModeChange;
+        inputActions.Player.MoveModeChange.canceled += OnMoveModeChange;
         inputActions.Player.Attack.performed += OnAttack;
     }
 
     private void OnDisable()
     {
         inputActions.Player.Attack.performed -= OnAttack;
+        inputActions.Player.MoveModeChange.performed -= OnMoveModeChange;
+        inputActions.Player.MoveModeChange.canceled -= OnMoveModeChange;
         inputActions.Player.Move.performed -= OnMove;
         inputActions.Player.Move.canceled -= OnMove;
         inputActions.Disable();
@@ -48,6 +58,8 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         cc.Move(currentSpeed * Time.deltaTime * inputDir);
+
+        player.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10 * Time.deltaTime);
     }
 
     private void OnMove(InputAction.CallbackContext context)
@@ -59,7 +71,7 @@ public class PlayerController : MonoBehaviour
         inputDir.y = 0.0f;
         inputDir.z = input.y;
 
-        Debug.Log($"{inputDir.x} {inputDir.z}");
+        //Debug.Log($"{inputDir.x} {inputDir.z}");
         
         if (!context.canceled)
         {
@@ -73,8 +85,8 @@ public class PlayerController : MonoBehaviour
             {
                 anim.SetFloat("Speed", 0.5f);   // walk모드면 걷는 애니메이션
 
-                anim.SetFloat("InputY", inputDir.z);
-                anim.SetFloat("InputX", inputDir.x);
+                //anim.SetFloat("InputY", inputDir.z);
+                //anim.SetFloat("InputX", inputDir.x);
             }
             else
             {
@@ -85,11 +97,40 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            inputDir = Vector3.zero;
+            //inputDir = Vector3.zero;           
             anim.SetFloat("Speed", 0.0f);   // 입력이 안들어 왔으면 대기 애니메이션.
         }
 
     }
+
+    private void OnMoveModeChange(InputAction.CallbackContext _)
+    {
+        isMoveChange = !isMoveChange;
+        MoveChange(isMoveChange);
+    }
+
+    void MoveChange(bool change)
+    {
+        if (change)
+        {
+            moveMode = MoveMode.Run;
+            currentSpeed = runSpeed;
+            if (inputDir != Vector3.zero)
+            {
+                anim.SetFloat("Speed", 1.0f);   // 움직이는 중일 때만 재생하는 애니메이션 변경
+            }
+        }
+        else
+        {
+            moveMode = MoveMode.Walk;
+            currentSpeed = walkSpeed;
+            if (inputDir != Vector3.zero)
+            {
+                anim.SetFloat("Speed", 0.5f);   // 움직이는 중일 때만 재생하는 애니메이션 변경
+            }
+        }
+    }
+
 
     private void OnAttack(InputAction.CallbackContext context)
     {
